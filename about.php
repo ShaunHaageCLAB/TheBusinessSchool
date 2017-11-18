6e346e497249769bcd646c6959ff870c5e01c2a0
Andy <afairlie@gmail.com>
2017-11-18 20:42:47 +0200

    Added about page

---
+++ about.php
@@ -0,0 +1,29 @@
+<?php include("includes/sharepoint/head.php"); ?>
+<?php include("includes/template/head.php"); ?>
+<title>About Pages</title>
+<?php include("includes/sharepoint/header.php"); ?>
+
+<div class="container / content__wrapper">
+    <div class="row">
+        <?php include("includes/template/sidebar.php"); ?>
+
+        <div class="col-sm-8 ">
+            <h1 class="h2">Headline - H1</h1>
+            <p>Throughout this site and all our communication, you will see reference to The Independent Institute of Education (The IIE). So who is The IIE and why are we important?</p>
+            <p>The Independent Institute of Education is South Africa’s largest registered and accredited private provider of higher education. Varsity College, a brand of The IIE, leads in the provision of these exciting learning initiatives of The IIE. The IE is registered with the Department of Higher Education and Training as a private higher education provider, has more than 57 qualifications accredited by the Higher Education Quality Committee and is a subsidiary of ADvTECH Ltd, a public company listed on the Johannesburg Stock Exchange. This scale and status assures our leading position in higher education.</p>
+        </div>
+
+    </div>
+
+    <h1 class="text-center">View Our brochures online</h1>
+
+</div>
+
+
+<?php include("includes/homepage/5-testimonials.php"); ?>
+
+
+
+<?php include("includes/sharepoint/footer.php"); ?>
+</body>
+</html>
